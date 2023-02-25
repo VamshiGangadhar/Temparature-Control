1dc4469c377cb5cf921eeeb29b533b5071c6739b
VamshiGangadhar <80575671+VamshiGangadhar@users.noreply.github.com>
2023-02-25 15:06:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import TempControl from "./components/TempControl";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <TempControl />
+    </div>
+  );
+}
+
+export default App;
